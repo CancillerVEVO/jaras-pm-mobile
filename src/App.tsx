bef1eeadecae6ac1aec55e9bbeadc9a811d0b003
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2024-02-19 22:40:35 -0600

    dev env setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>NO HAY DIVS????</Text>
+      <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+      <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+      <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       <StatusBar style="auto" />
     </View>
   );
