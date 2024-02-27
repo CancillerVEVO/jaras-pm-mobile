f4c23c61929d9ca1f309caf6e09d182f74d30ee8
cancillerVEVO <emma.lopez.ojeda@gmail.com>
2024-02-26 23:37:12 -0600

    expo sqlite installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>NO HAY DIVS????</Text>
